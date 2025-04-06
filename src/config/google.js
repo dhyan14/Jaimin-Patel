cb3235a2bdd9c5d5d106feac1242242c60326da8
dhyan14 <dhyanjain341988@gmail.com>
2025-04-06 15:37:48 +0530

    GGG

---
+++ src/config/google.js
@@ -0,0 +1,6 @@
+export const googleConfig = {
+  clientId: process.env.GOOGLE_CLIENT_ID,
+  clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  redirectUri: process.env.GOOGLE_REDIRECT_URI,
+  folderId: process.env.GOOGLE_FOLDER_ID
+};
